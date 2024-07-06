580b0111568a24fa8515e63c3ddc6bbec390f1c9
AnuragRaj11 <Araj59519@gmail.com>
2024-07-06 20:44:38 +0530

    To_Do_List

---
+++ to-do-list-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import navbar from './components/navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     < navbar/>
+      <div className=''></div>
+    </>
+  )
+}
+
+export default App
---
+++ to-do-list-project/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <nav className="flex justify-center ">
+        <div className="logo">
+            <span className="font-bold text-xl mx-7">iTask</span>
+        </div>
+        <ul className="flex gap-7 mx-9">
+            <li className="cursor-pointer "></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default navbar
