fd5f2956da5af592245332a099b86dee26951a6a
AnuragRaj11 <Araj59519@gmail.com>
2024-07-06 22:28:51 +0530

    R

--- to-do-list-project/src/App.jsx
+++ to-do-list-project/src/App.jsx
@@ -2,12 +2,36 @@ import { useState } from 'react'
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Todo, setTodo] = useState("")
+  const [Todos, setTodos] = useState([])
+  const [showfinished, setfinished] = useState(true)
+
+  useEffect(() => {
+    let todoString = localStorage.getItem("todos")
+    if (todoString) {
+      let todos = JSON.parse(localStorage.getItem("todos"))
+    }
+  }, [])
+
+  const saveToLS = (params) => {
+    localStorage.setItem("todos", JSON.stringify(params))
+  }
+
+  const tooglefinished = (e) => {
+    setfinished(!showfinished)
+  }
 
   return (
     <>
-     < Navbar/>
-      <div className='container mx-auto my-4  '></div>
+      < Navbar />
+      <div className='container mx-auto my-4 rounded-xl p-4 bg-violet-400 min-h-[90vh] '>
+        <div className='addTodo'>
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <div className='flex'>
+            <input onChange= type="text" />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
